7e599a9d17d214f29c157d8d1abcaa77c2e69d3c
Rehtt <dsreshiram@gmail.com>
2024-04-04 15:23:33 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"regexp"
+	"strconv"
+
+	"github.com/miekg/dns"
+)
+
+func main() {
+	// 监听 127.0.0.1:5345
+	addr := &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 5345}
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Printf("DNS server listening on %s\n", addr)
+
+	for {
+		buf := make([]byte, 512)
+		n, remoteAddr, _ := conn.ReadFromUDP(buf)
+		// 为每个请求启动一个goroutine进行处理,实现并发
+		go handleDNSRequest(conn, remoteAddr, buf[:n])
+	}
+}
+
+func handleDNSRequest(conn *net.UDPConn, remoteAddr *net.UDPAddr, reqBytes []byte) {
+	// 解析请求数据
+	req := new(dns.Msg)
+	req.Unpack(reqBytes)
+
+	// 构造响应消息
+	resp := new(dns.Msg)
+	resp.SetReply(req)
+
+	// 遍历请求的问题
+	for _, q := range req.Question {
+		// 如果是A记录查询
+		if q.Qtype == dns.TypeA {
+			ip := parseIP(q.Name)
+			// 如果域名匹配特定模式,直接返回对应IP
+			if ip != nil {
+				resp.Answer = append(resp.Answer, &dns.A{
+					Hdr: dns.RR_Header{Name: q.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+					A:   ip,
+				})
+				continue
+			}
+		}
+		// 将其他查询请求转发到上游DNS服务器 8.8.8.8
+		systemResp, err := dns.Exchange(req, "8.8.8.8:53")
+		if err == nil {
+			// 将上游DNS服务器的应答添加到响应中
+			resp.Answer = append(resp.Answer, systemResp.Answer...)
+		}
+		fmt.Println(err)
+	}
+
+	// 将响应打包并发送
+	respBytes, _ := resp.Pack()
+	conn.WriteToUDP(respBytes, remoteAddr)
+}
+
+// 匹配类似：1.1.1.1.domain.com 的特殊域名
+var ipv4Regex = regexp.MustCompile(`^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.([\w-]+\.)+\w+\.`)
+
+func parseIP(name string) net.IP {
+	// 用正则表达式匹配域名
+	if match := ipv4Regex.FindStringSubmatch(name); match != nil {
+		ip := make(net.IP, 4)
+		// 解析IP地址的4个部分
+		for i := 0; i < 4; i++ {
+			n, _ := strconv.Atoi(match[i+1])
+			ip[i] = byte(n)
+		}
+		return ip
+	}
+	return nil
+}
